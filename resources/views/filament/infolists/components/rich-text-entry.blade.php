4c4de9929f3138b2a061f4135602b6ccca932b8b
Joaquim Perez <jivkim@gmail.com>
2025-09-18 15:33:20 +0200

    fix(rich-text-entry): don't render component if there are no template

--- resources/views/filament/infolists/components/rich-text-entry.blade.php
+++ resources/views/filament/infolists/components/rich-text-entry.blade.php
@@ -1,11 +1,14 @@
 @php
     use Filament\Forms\Components\RichEditor\RichContentRenderer;
+    $richTextJson = $getState();
 @endphp
 
+@if($richTextJson)
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     <div class='prose dark:prose-invert border border-current/30 p-4 rounded-lg' {{ $getExtraAttributeBag() }}>
-        {{ RichContentRenderer::make($getState())->mergeTags([
+        {{ RichContentRenderer::make($richTextJson)->mergeTags([
             'name' => 'Joe',
         ]) }}
     </div>
 </x-dynamic-component>
+@endif
