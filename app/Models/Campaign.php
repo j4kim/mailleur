8402415bb4ef28c5a0f582c60f46014bda7eff5e
Joaquim Perez <jivkim@gmail.com>
2025-09-19 16:17:30 +0200

    fix(Campaign): values after reject to reset indexes

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -37,7 +37,7 @@ class Campaign extends Model
             if ($header->doesntContain('email')) {
                 abort(422, 'The file must contain an "email" header');
             }
-            $this->columns = collect($this->columns)->concat($header)->unique()->values()->reject('email');
+            $this->columns = collect($this->columns)->concat($header)->unique()->reject('email')->values();
             $this->save();
             foreach ($csv as $index => $row) {
                 $row = collect($row)->mapWithKeys(fn($v, $k) => [strtolower($k) => $v]);
