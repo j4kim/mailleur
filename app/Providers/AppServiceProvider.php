e885f72adb0af42aeb1f696a3395d0b6d6fae561
Joaquim Perez <jivkim@gmail.com>
2025-09-20 01:33:53 +0200

    enable db queries logging, use daily as default channel, create a new channel for db queries, create a new config logging.db_queries, disable pail in composer run dev, use file driver for cache and session.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +22,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('logging.db_queries')) {
+            DB::listen(function ($query) {
+                if (str_starts_with($query->sql, "select * from \"jobs\"")) return;
+                $log = log10($query->time) + 1;
+                $magnitude = $log < 0 ? 0 : round($log);
+                $warnChars = str_pad(str_repeat(">", $magnitude), 5, " ");
+                $seconds = number_format($query->time / 1000, 3);
+                $bindings = json_encode($query->bindings);
+                Log::channel("queries")->info(
+                    "$warnChars ($seconds) | $query->sql | $bindings"
+                );
+            });
+        }
+
         Model::unguard();
     }
 }
--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'queries' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/queries.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => env('LOG_DAILY_DAYS', 14),
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
@@ -89,7 +97,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -128,5 +136,7 @@ return [
         ],
 
     ],
+
+    'db_queries' => env('DB_LOG_QUERIES'),
 
 ];
