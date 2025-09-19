9672d7d57a48a63cc5cd4c25c5682ca7fd41f0d6
Joaquim Perez <jivkim@gmail.com>
2025-09-19 16:03:15 +0200

    vite.config.js: remove default inputs (files have been removed)

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
                 "resources/css/filament/admin/theme.css",
             ],
             refresh: true,
