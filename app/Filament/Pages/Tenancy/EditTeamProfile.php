7ff95a2481213fa6665f81bc037f3f2bcb0d64fe
Joaquim Perez <jivkim@gmail.com>
2025-09-22 17:36:46 +0200

    feat(EditTeamProfile): disable form if not team admin

--- app/Filament/Pages/Tenancy/EditTeamProfile.php
+++ app/Filament/Pages/Tenancy/EditTeamProfile.php
@@ -2,17 +2,26 @@
 
 namespace App\Filament\Pages\Tenancy;
 
+use Filament\Actions\Action;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Tenancy\EditTenantProfile;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Auth;
 
 class EditTeamProfile extends EditTenantProfile
 {
     protected string $view = 'filament.edit-team';
 
+    private bool $iAmAdmin;
+
     public static function getLabel(): string
     {
         return 'Team settings';
+    }
+
+    public function beforeFill()
+    {
+        $this->iAmAdmin = Auth::user()->isAdminOf($this->tenant);
     }
 
     public function form(Schema $schema): Schema
@@ -20,6 +29,11 @@ class EditTeamProfile extends EditTenantProfile
         return $schema
             ->components([
                 TextInput::make('name'),
-            ]);
+            ])->disabled(!$this->iAmAdmin);
+    }
+
+    protected function getSaveFormAction(): Action
+    {
+        return parent::getSaveFormAction()->hidden(!$this->iAmAdmin);;
     }
 }
