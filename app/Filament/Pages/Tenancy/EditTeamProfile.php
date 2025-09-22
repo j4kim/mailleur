8043182832130f9d95258fdf2f55db75d79fa4c2
Joaquim Perez <jivkim@gmail.com>
2025-09-22 17:46:26 +0200

    [wip] EditTeamProfile: prepare smtp config inputs

--- app/Filament/Pages/Tenancy/EditTeamProfile.php
+++ app/Filament/Pages/Tenancy/EditTeamProfile.php
@@ -5,6 +5,9 @@ namespace App\Filament\Pages\Tenancy;
 use Filament\Actions\Action;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Tenancy\EditTenantProfile;
+use Filament\Schemas\Components\Flex;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Facades\Auth;
 
@@ -29,6 +32,18 @@ class EditTeamProfile extends EditTenantProfile
         return $schema
             ->components([
                 TextInput::make('name'),
+                Section::make('SMTP config')
+                    ->columns([
+                        'sm' => 2,
+                        'xl' => 4,
+                    ])
+                    ->schema([
+                        TextInput::make('host'),
+                        TextInput::make('port'),
+                        TextInput::make('username'),
+                        TextInput::make('password'),
+                    ])
+                    ->hidden(!$this->iAmAdmin),
             ])->disabled(!$this->iAmAdmin);
     }
 
