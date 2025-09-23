abfc13c71e8f92e67e0e82a838f359f0a4a0514b
Joaquim Perez <jivkim@gmail.com>
2025-09-23 21:32:05 +0200

    layout(rich-text-entry): use fi-section class

--- resources/views/filament/infolists/components/rich-text-entry.blade.php
+++ resources/views/filament/infolists/components/rich-text-entry.blade.php
@@ -1,18 +1,18 @@
 @php
     use Filament\Forms\Components\RichEditor\RichContentRenderer;
     $template = $getState();
-    $rendered = "";
+    $rendered = '';
     if ($template) {
         $mergeTags = collect($record->columns)
             ->push('email')
-            ->mapWithKeys(fn($c) => [$c => "{{ $c }}"])
+            ->mapWithKeys(fn($c) => [$c => '{{ $c }}'])
             ->toArray();
         $rendered = RichContentRenderer::make($template)->mergeTags($mergeTags);
     }
 @endphp
 
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <div class='prose bg-white dark:bg-gray-900 dark:prose-invert border border-current/10 py-4 px-6 rounded-xl' {{ $getExtraAttributeBag() }}>
+    <div class='prose fi-section text-gray-950 dark:text-white py-4 px-6' {{ $getExtraAttributeBag() }}>
         {{ $rendered }}
     </div>
 </x-dynamic-component>
