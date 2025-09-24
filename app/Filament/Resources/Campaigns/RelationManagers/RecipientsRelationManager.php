dc687ba3984479c493725c6c978c5551eefa9354
Joaquim Perez <jivkim@gmail.com>
2025-09-24 14:09:54 +0200

    refactor(RecipientsRelationManager): move ImportRecipients in a class

--- app/Filament/Resources/Campaigns/RelationManagers/RecipientsRelationManager.php
+++ app/Filament/Resources/Campaigns/RelationManagers/RecipientsRelationManager.php
@@ -3,17 +3,15 @@
 namespace App\Filament\Resources\Campaigns\RelationManagers;
 
 use App\Enums\RecipientStatus;
+use App\Filament\Actions\Campaign\ImportRecipients;
 use App\Filament\Actions\Recipient as Actions;
 use App\Models\Campaign;
-use App\Models\Recipient;
-use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
@@ -21,11 +19,6 @@ use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
-use Livewire\Component;
-use Throwable;
-
-use function App\Tools\errorNotif;
-use function App\Tools\successNotif;
 
 class RecipientsRelationManager extends RelationManager
 {
@@ -98,22 +91,7 @@ class RecipientsRelationManager extends RelationManager
             ])
             ->headerActions([
                 CreateAction::make()->outlined(),
-                Action::make('import recipients')
-                    ->schema([
-                        FileUpload::make('csv_file')
-                            ->label("CSV file")
-                            ->required()
-                            ->acceptedFileTypes(['text/csv']),
-                    ])->action(function (array $data, Action $action, Component $livewire) use ($campaign) {
-                        try {
-                            $campaign->importCsv($data['csv_file']);
-                            successNotif('Recipients imported successfully');
-                            return $livewire->redirect(request()->header('Referer'));
-                        } catch (Throwable $e) {
-                            errorNotif($e->getMessage());
-                            $action->cancel();
-                        }
-                    }),
+                ImportRecipients::make()->init($campaign),
             ])
             ->recordActions([
                 ActionGroup::make([
