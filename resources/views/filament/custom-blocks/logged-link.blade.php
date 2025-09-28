e514d9083383ed6cf91790a3b0ec19481093fb86
Joaquim Perez <jivkim@gmail.com>
2025-09-28 23:06:18 +0200

    logged-link: button style

--- resources/views/filament/custom-blocks/logged-link.blade.php
+++ resources/views/filament/custom-blocks/logged-link.blade.php
@@ -1,3 +1,13 @@
-<a href="{{ $config['url'] }}">
-    {{ @$config['text'] }}
-</a>
+<div style="text-align: center">
+    <a href="{{ $config['url'] }}"
+        style="
+            display: inline-block;
+            padding: 0.5em 1em;
+            background-color: #057ec4;
+            color: white;
+            text-decoration: none;
+        "
+    >
+        {{ @$config['text'] }}
+    </a>
+</div>
