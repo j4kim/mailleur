54876795894e6ee5cfe10195a4724c127d4cefee
Joaquim Perez <jivkim@gmail.com>
2025-10-26 21:58:14 +0100

    generate and render mail body in mail render route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Models\Recipient;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/mail/{recipient}/render', function (Recipient $recipient) {
+    if (!$recipient->mail_body) {
+        $recipient->mail_body = $recipient->generateMailBody();
+    }
+    $recipient->rendered_mail_body = $recipient->renderMailBody();
     return new CampaignMail($recipient);
 })->middleware('auth');
 
