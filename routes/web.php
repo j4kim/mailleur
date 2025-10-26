22e69c76a7234357d8388c33bb30186fd9676df6
Joaquim Perez <jivkim@gmail.com>
2025-10-26 21:18:15 +0100

    New redirect route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Mail\CampaignMail;
+use App\Models\Link;
 use App\Models\Recipient;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/mail/{recipient}/render', function (Recipient $recipient) {
     return new CampaignMail($recipient);
 })->middleware('auth');
+
+Route::get('redirect/{token}', function (string $token) {
+    $link = Link::where('token', $token)->firstOrFail();
+    return redirect($link->url);
+});
