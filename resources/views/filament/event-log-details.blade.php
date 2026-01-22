0606c7218f348dc9b90d2ee1d79e72dbfbe48832
Joaquim Perez <jivkim@gmail.com>
2026-01-23 00:44:09 +0100

    layout(event-log-details)

--- resources/views/filament/event-log-details.blade.php
+++ resources/views/filament/event-log-details.blade.php
@@ -12,18 +12,14 @@
             $old = RecipientStatus::from($meta['old']);
             $new = RecipientStatus::from($meta['new']);
         @endphp
-        From
-        <x-filament::badge :color="$old->getColor()" size="sm">
-            {{ $old->getLabel() }}
-        </x-filament::badge>
-        to
+        From {{ $old->getLabel() }} to
         <x-filament::badge :color="$new->getColor()" size="sm">
             {{ $new->getLabel() }}
         </x-filament::badge>
     @elseif ($eventLog->type === EventLogType::LinkClicked)
         {{ @$eventLog->meta['url'] }}
     @elseif ($eventLog->type === EventLogType::MailScheduled)
-        to be sent at {{ Carbon::make($eventLog->meta['to_be_sent_at'])->format('d.m.Y H:i') }}
+        To be sent at {{ Carbon::make($eventLog->meta['to_be_sent_at'])->format('d.m.Y H:i') }}
     @else
         <x-filament::link :href="route('event-log-details', $eventLog)" target="_blank">
             Details
